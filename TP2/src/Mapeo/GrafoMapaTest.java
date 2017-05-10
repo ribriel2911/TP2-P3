173f8441c202b8d5e23ae9939bd49e5d81debd8c
Ricardo Dominguez <richard291190@gmail.com>
2017-05-10 02:05:14 -0300

    Empezando la interfaz grafica

--- TP2/src/Mapeo/GrafoMapaTest.java
+++ TP2/src/Mapeo/GrafoMapaTest.java
@@ -5,56 +5,51 @@ import java.util.Set;
 
 import org.junit.Test;
 
-public class GrafoMapaTest
-{
+public class GrafoMapaTest{
+
+	//Tests propios de la clase Grafo trabajada en clase
+
 	@Test
-	public void agregarRutaTest()
-	{
+	public void agregarRutaTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.agregarRuta(0, 2,1,false);
 		assertTrue( mapa.existeRuta(0, 2));
 	}
 	
 	@Test
-	public void agregarIndicesInvertidosTest()
-	{
+	public void agregarIndicesInvertidosTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.agregarRuta(1, 3,1,false);
 		assertTrue( mapa.existeRuta(3, 1) );
 	}
 	
 	@Test
-	public void rutaInexistenteTest()
-	{
+	public void rutaInexistenteTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.agregarRuta(1, 3,1,false);
 		assertFalse( mapa.existeRuta(1, 2) );
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void construccionErroneaTest()
-	{
+	public void construccionErroneaTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.agregarRuta(1, 4,1,false);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void idCiudadNegativaTest()
-	{
+	public void idCiudadNegativaTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.agregarRuta(-1, 2,1,false);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void sinLoopsTest()
-	{
+	public void sinLoopsTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.agregarRuta(3, 3,1,false);
 	}
 	
 	@Test
-	public void destruirRutaTest()
-	{
+	public void destruirRutaTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.agregarRuta(1, 2,1,false);
 
@@ -63,22 +58,19 @@ public class GrafoMapaTest
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void eliminarExcedidoTest()
-	{
+	public void eliminarExcedidoTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.eliminarRuta(0, 4,1,false);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void eliminarNegativoTest()
-	{
+	public void eliminarNegativoTest(){
 		Mapa mapa = new Mapa(4);
 		mapa.eliminarRuta(3, -1,1,false);
 	}
 
 	@Test
-	public void gradoTest()
-	{
+	public void gradoTest(){
 		Mapa rueda = construirRueda();
 		
 		assertEquals(5, rueda.getGrado(5));
@@ -86,59 +78,56 @@ public class GrafoMapaTest
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void gradoInvalidoTest()
-	{
+	public void gradoInvalidoTest(){
 		Mapa rueda = construirRueda();
 		rueda.getGrado(-1);	
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void gradoExcedidoTest()
-	{
+	public void gradoExcedidoTest(){
 		Mapa rueda = construirRueda();
 		rueda.getGrado(6);		
 	}
 	
 	@Test
-	public void vecinosUniversalTest()
-	{
+	public void vecinosUniversalTest(){
 		Mapa rueda = construirRueda();
 		Set<Integer> vecinos = rueda.getVecinos(5);
 		Assert.coinciden(vecinos, new int[] {0, 1, 2, 3, 4});
 	}
 	
 	@Test
-	public void vecinosRegularTest()
-	{
+	public void vecinosRegularTest(){
+		
 		Mapa rueda = construirRueda();
 		Set<Integer> vecinos = rueda.getVecinos(3);
 		Assert.coinciden(vecinos, new int[] {2, 4, 5});
 	}
 	
 	@Test
-	public void aisladoTest()
-	{
+	public void aisladoTest(){
+		
 		Mapa mapa = new Mapa(3);
 		Set<Integer> vecinos = mapa.getVecinos(1);
 		Assert.coinciden(vecinos, new int[] {});
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void vecinosExcedidoTest()
-	{
+	public void vecinosExcedidoTest(){
+		
 		Mapa mapa = new Mapa(3);
 		mapa.getVecinos(3);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void vecinosNegativoTest()
-	{
+	public void vecinosNegativoTest(){
+		
 		Mapa mapa = new Mapa(3);
 		mapa.getVecinos(-1);
 	}
 	
-	private Mapa construirRueda()
-	{
+	private Mapa construirRueda(){
+		
 		Mapa rueda = new Mapa(6);
 		rueda.agregarRuta(0, 1,1,false);
 		rueda.agregarRuta(1, 2,1,false);
